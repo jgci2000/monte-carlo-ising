bec61e6266e919d3cbf06853d852fae0c509246f
João Inácio <jgci2000@hotmail.com>
2021-11-26 15:56:17 +0100

    omp test version

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -1,21 +1,26 @@
+#include <chrono>
+#include <cmath>
 
-#include <iostream>
-// #include <cmath>
+#include <omp.h>
 
 #include "system.h"
 #include "wl.h"
 #include "rng.h"
 
 int main(int argv, char **argc) {
+    omp_set_num_threads(4);
 
-    RNG rng(20);
+    std::printf("Executing on %d threads \n", omp_get_num_threads());
 
-    System ising_lattice(4, 2, "SS");
-    ising_lattice.init_spins_random(rng);
+    #pragma omp parallel
+    {
+        RNG rng(20 * omp_get_thread_num());
+        System ising_lattice(4, 2, "SS");
+        WL wang_landau(exp(1), 1 + pow(10, -8), 0.9, rng, ising_lattice);
 
-    WL wang_landau(exp(1), 1 + pow(10, -8), 0.9, rng, ising_lattice);
-
-    wang_landau.simulate(10000);
-
+        wang_landau.simulate(1000, omp_get_thread_num(), false);
+        wang_landau.write_to_file(std::to_string(omp_get_thread_num()) + "_JDOS.txt", false);
+    }
+    
     return 0;
 }
