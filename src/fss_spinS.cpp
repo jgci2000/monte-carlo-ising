bba648cd325f632bfd81226a81526ea91d88ffe4
João Inácio <jgci2000@hotmail.com>
2021-12-20 18:22:53 +0000

    small optimizations

--- src/fss_spinS.cpp
+++ src/fss_spinS.cpp
@@ -33,52 +33,52 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             this->idx_M_max);
     }
     
-    for (int q = 1; q <= this->idx_M_max; q++)
+    for (int q = 1; q <= this->idx_M_max; ++q)
     {
         auto q_start = std::chrono::steady_clock::now();
 
         long long *hist2 = new long long[this->ising_lattice->NE * this->ising_lattice->line_size_Npos.at(q)];
 
-        for (int i = 0; i < this->ising_lattice->NE * this->ising_lattice->line_size_Npos.at(q); i++)
+        for (int i = 0; i < this->ising_lattice->NE * this->ising_lattice->line_size_Npos.at(q); ++i)
         {
             hist2[i] = 0;
         }
 
         // Random config at q
 
-        for (int i = 0; i < this->ising_lattice->N_atm; i++)
+        for (int i = 0; i < this->ising_lattice->N_atm; ++i)
             this->ising_lattice->spins_vector[i] = this->ising_lattice->spins_values[0];
         int E_config =  - this->ising_lattice->max_E;
         int idx_E_config = this->ising_lattice->energies[E_config];
 
-        for (int i = 0; i < this->ising_lattice->N_atm; i++)
+        for (int i = 0; i < this->ising_lattice->N_atm; ++i)
             SPM[i] = 0;
 
         for (int idx = 1; idx <= q; idx++)
         {
             std::vector<int> flip_list;
-            for (int i = 0; i < this->ising_lattice->N_atm; i++)
+            for (int i = 0; i < this->ising_lattice->N_atm; ++i)
                 if (SPM[i] + 1 < this->ising_lattice->Sz)
                     flip_list.push_back(i);
             int flipped_idx = flip_list.at(this->rng->rand() % flip_list.size());
 
             int E_old = 0;
-            for (int a = 0; a < this->ising_lattice->NN; a++)
+            for (int a = 0; a < this->ising_lattice->NN; ++a)
                 E_old += - this->ising_lattice->spins_vector[flipped_idx] * this->ising_lattice->spins_vector[this->ising_lattice->NN_table[flipped_idx * this->ising_lattice->NN + a]];
 
             SPM[flipped_idx]++;
             this->ising_lattice->spins_vector[flipped_idx] = this->ising_lattice->spins_values[SPM[flipped_idx]];
 
             int E_new = 0;
-            for (int a = 0; a < this->ising_lattice->NN; a++)
+            for (int a = 0; a < this->ising_lattice->NN; ++a)
                 E_new += - this->ising_lattice->spins_vector[flipped_idx] * this->ising_lattice->spins_vector[this->ising_lattice->NN_table[flipped_idx * this->ising_lattice->NN + a]];
 
             E_config = E_config - E_old + E_new;
         }
 
         std::vector<int> counter; counter.assign(this->ising_lattice->Sz, 0);
-        for (int i = 0; i < this->ising_lattice->N_atm; i++)
-            for (int j = 0; j < this->ising_lattice->Sz; j++)
+        for (int i = 0; i < this->ising_lattice->N_atm; ++i)
+            for (int j = 0; j < this->ising_lattice->Sz; ++j)
                 if (SPM[i] == j)
                     counter[j]++;
 
@@ -86,7 +86,7 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
         int idx_Npos;
         for (idx_Npos = 0; idx_Npos < this->ising_lattice->line_size_Npos.at(q) * this->ising_lattice->Sz; idx_Npos += this->ising_lattice->Sz)
         {
-            for (int i = 0; i < this->ising_lattice->Sz; i++)
+            for (int i = 0; i < this->ising_lattice->Sz; ++i)
                 if (counter[i] == this->ising_lattice->Npos[q][i + idx_Npos])
                     counter2++;
 
@@ -106,10 +106,10 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
         // Scan the first config
         // printf("HERE_before! \n");
 
-        for (int x = 1; x < this->ising_lattice->Sz; x++)
+        for (int x = 1; x < this->ising_lattice->Sz; ++x)
         {
             std::vector<int> flip_list;
-            for (int i = 0; i < this->ising_lattice->N_atm; i++)
+            for (int i = 0; i < this->ising_lattice->N_atm; ++i)
                 if (SPM[i] + 1 <= this->ising_lattice->Sz - x)
                     flip_list.push_back(i);
 
@@ -117,16 +117,16 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             //     printf("%d ", flip_list.at(i));
             // printf("\n");
 
-            for (int flip_idx = 0; flip_idx < flip_list.size(); flip_idx++)
+            for (int flip_idx = 0; flip_idx < flip_list.size(); ++flip_idx)
             {
                 std::vector<int> SPM_tmp = SPM;
 
                 int E_tmp1 = 0;
-                for (int a = 0; a < this->ising_lattice->NN; a++)
+                for (int a = 0; a < this->ising_lattice->NN; ++a)
                     E_tmp1 += - this->ising_lattice->spins_vector[flip_list.at(flip_idx)] * this->ising_lattice->spins_vector[this->ising_lattice->NN_table[flip_list.at(flip_idx) * this->ising_lattice->NN + a]];
 
                 int E_tmp2 = 0;
-                for (int a = 0; a < this->ising_lattice->NN; a++)
+                for (int a = 0; a < this->ising_lattice->NN; ++a)
                     E_tmp2 += - this->ising_lattice->spins_values[SPM[flip_list.at(flip_idx)] + x] * this->ising_lattice->spins_vector[this->ising_lattice->NN_table[flip_list.at(flip_idx) * this->ising_lattice->NN + a]];
 
                 int E_tmp3 = E_config - E_tmp1 + E_tmp2;
@@ -135,8 +135,8 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
                 SPM_tmp[flip_list.at(flip_idx)] = SPM[flip_list.at(flip_idx)] + x;
 
                 std::vector<int> counter; counter.assign(this->ising_lattice->Sz, 0);
-                for (int i = 0; i < this->ising_lattice->N_atm; i++)
-                    for (int j = 0; j < this->ising_lattice->Sz; j++)
+                for (int i = 0; i < this->ising_lattice->N_atm; ++i)
+                    for (int j = 0; j < this->ising_lattice->Sz; ++j)
                         if (SPM_tmp[i] == j)
                             counter[j]++;
 
@@ -144,7 +144,7 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
                 int idx_Npos;
                 for (idx_Npos = 0; idx_Npos < this->ising_lattice->line_size_Npos.at(q + x) * this->ising_lattice->Sz; idx_Npos += this->ising_lattice->Sz)
                 {
-                    for (int i = 0; i < this->ising_lattice->Sz; i++)
+                    for (int i = 0; i < this->ising_lattice->Sz; ++i)
                         if (counter[i] == this->ising_lattice->Npos[q + x][i + idx_Npos])
                             counter2++;
 
@@ -180,7 +180,7 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             if (!accepted)
             {
                 new_SPM = SPM;
-                for (int i = 0; i < this->ising_lattice->N_atm; i++)
+                for (int i = 0; i < this->ising_lattice->N_atm; ++i)
                     new_spins_vector[i] =  this->ising_lattice->spins_vector[i];
             }
             int new_E_config = 0;
@@ -192,11 +192,11 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             int flipped_idx1 = this->rng->rand() % this->ising_lattice->N_atm;
 
             int E_old = 0;
-            for (int a = 0; a < this->ising_lattice->NN; a++)
+            for (int a = 0; a < this->ising_lattice->NN; ++a)
                 E_old += - new_spins_vector[flipped_idx1] * new_spins_vector[this->ising_lattice->NN_table[flipped_idx1 * this->ising_lattice->NN + a]];
 
             std::vector<int> SPM_end_list;
-            for (int i = 0; i < this->ising_lattice->Sz; i++)
+            for (int i = 0; i < this->ising_lattice->Sz; ++i)
                 if (i != SPM[flipped_idx1])
                     SPM_end_list.push_back(i);
 
@@ -207,7 +207,7 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             new_SPM[flipped_idx1] = SPM_end;
 
             int E_new = 0;
-            for (int a = 0; a < this->ising_lattice->NN; a++)
+            for (int a = 0; a < this->ising_lattice->NN; ++a)
                 E_new += - new_spins_vector[flipped_idx1] * new_spins_vector[this->ising_lattice->NN_table[flipped_idx1 * this->ising_lattice->NN + a]];
             new_E_config = E_config - E_old + E_new;
 
@@ -216,27 +216,27 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             // Choose another one to flip back
 
             std::vector<int> flip_list;
-            for (int i = 0; i < this->ising_lattice->N_atm; i++)
+            for (int i = 0; i < this->ising_lattice->N_atm; ++i)
                 if (new_SPM[i] + 1 - SPM_dif >= 1 && new_SPM[i] + 1 - SPM_dif <= this->ising_lattice->Sz)
                     flip_list.push_back(i);
 
             int flipped_idx2 = flip_list.at(this->rng->rand() % flip_list.size());
 
             E_old = 0;
-            for (int a = 0; a < this->ising_lattice->NN; a++)
+            for (int a = 0; a < this->ising_lattice->NN; ++a)
                 E_old += - new_spins_vector[flipped_idx2] * new_spins_vector[this->ising_lattice->NN_table[flipped_idx2 * this->ising_lattice->NN + a]];
 
             new_SPM[flipped_idx2] = new_SPM[flipped_idx2] - SPM_dif;
             new_spins_vector[flipped_idx2] = this->ising_lattice->spins_values[new_SPM[flipped_idx2]];
 
             E_new = 0;
-            for (int a = 0; a < this->ising_lattice->NN; a++)
+            for (int a = 0; a < this->ising_lattice->NN; ++a)
                 E_new += - new_spins_vector[flipped_idx2] * new_spins_vector[this->ising_lattice->NN_table[flipped_idx2 * this->ising_lattice->NN + a]];
             new_E_config = new_E_config - E_old + E_new;
 
             std::vector<int> counter; counter.assign(this->ising_lattice->Sz, 0);
-            for (int i = 0; i < this->ising_lattice->N_atm; i++)
-                for (int j = 0; j < this->ising_lattice->Sz; j++)
+            for (int i = 0; i < this->ising_lattice->N_atm; ++i)
+                for (int j = 0; j < this->ising_lattice->Sz; ++j)
                     if (new_SPM[i] == j)
                         counter[j]++;
 
@@ -244,7 +244,7 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             int idx_Npos;
             for (idx_Npos = 0; idx_Npos < this->ising_lattice->line_size_Npos.at(q) * this->ising_lattice->Sz; idx_Npos += this->ising_lattice->Sz)
             {
-                for (int i = 0; i < this->ising_lattice->Sz; i++)
+                for (int i = 0; i < this->ising_lattice->Sz; ++i)
                     if (counter[i] == this->ising_lattice->Npos[q][i + idx_Npos])
                         counter2++;
 
@@ -263,7 +263,7 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
 
             if (ratio >= 1 || this->rng->rand_uniform() < ratio || hist2[new_idx_E_config * this->ising_lattice->line_size_Npos.at(q) + new_idx_Npos_config] == 0)
             {
-                for (int i = 0; i < this->ising_lattice->N_atm; i++)
+                for (int i = 0; i < this->ising_lattice->N_atm; ++i)
                     this->ising_lattice->spins_vector[i] = new_spins_vector[i];
 
                 E_config = new_E_config;
@@ -283,23 +283,23 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
 
             if (hist2[idx_E_config * this->ising_lattice->line_size_Npos.at(q) + idx_Npos_config] < this->REP && k % this->skip == 0 || hist2[idx_E_config * this->ising_lattice->line_size_Npos.at(q) + idx_Npos_config] == 0)
             {
-                for (int x = 1; x < this->ising_lattice->Sz; x++)
+                for (int x = 1; x < this->ising_lattice->Sz; ++x)
                 {
                     std::vector<int> flip_list;
-                    for (int i = 0; i < this->ising_lattice->N_atm; i++)
+                    for (int i = 0; i < this->ising_lattice->N_atm; ++i)
                         if (SPM[i] + 1 <= this->ising_lattice->Sz - x)
                             flip_list.push_back(i);
 
-                    for (int flip_idx = 0; flip_idx < flip_list.size(); flip_idx++)
+                    for (int flip_idx = 0; flip_idx < flip_list.size(); ++flip_idx)
                     {
                         std::vector<int> SPM_tmp = SPM;
 
                         int E_tmp1 = 0;
-                        for (int a = 0; a < this->ising_lattice->NN; a++)
+                        for (int a = 0; a < this->ising_lattice->NN; ++a)
                             E_tmp1 += - this->ising_lattice->spins_vector[flip_list.at(flip_idx)] * this->ising_lattice->spins_vector[this->ising_lattice->NN_table[flip_list.at(flip_idx) * this->ising_lattice->NN + a]];
 
                         int E_tmp2 = 0;
-                        for (int a = 0; a < this->ising_lattice->NN; a++)
+                        for (int a = 0; a < this->ising_lattice->NN; ++a)
                             E_tmp2 += - this->ising_lattice->spins_values[SPM[flip_list.at(flip_idx)] + x] * this->ising_lattice->spins_vector[this->ising_lattice->NN_table[flip_list.at(flip_idx) * this->ising_lattice->NN + a]];
 
                         int E_tmp3 = E_config - E_tmp1 + E_tmp2;
@@ -308,8 +308,8 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
                         SPM_tmp[flip_list.at(flip_idx)] = SPM[flip_list.at(flip_idx)] + x;
 
                         std::vector<int> counter; counter.assign(this->ising_lattice->Sz, 0);
-                        for (int i = 0; i < this->ising_lattice->N_atm; i++)
-                            for (int j = 0; j < this->ising_lattice->Sz; j++)
+                        for (int i = 0; i < this->ising_lattice->N_atm; ++i)
+                            for (int j = 0; j < this->ising_lattice->Sz; ++j)
                                 if (SPM_tmp[i] == j)
                                     counter[j]++;
 
@@ -317,7 +317,7 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
                         int idx_Npos;
                         for (idx_Npos = 0; idx_Npos < this->ising_lattice->line_size_Npos.at(q + x) * this->ising_lattice->Sz; idx_Npos += this->ising_lattice->Sz)
                         {
-                            for (int i = 0; i < this->ising_lattice->Sz; i++)
+                            for (int i = 0; i < this->ising_lattice->Sz; ++i)
                                 if (counter[i] == this->ising_lattice->Npos[q + x][i + idx_Npos])
                                     counter2++;
 
@@ -336,38 +336,37 @@ void FSS::simulate_Sz_S(int run, bool verbose) {
             }
         
             k++;
-            // printf("%d \n", k);
         }
         
         // Normalize JDOS and output to console
 
         std::vector<long double> sum_JDOS_M_spin; sum_JDOS_M_spin.assign(this->ising_lattice->NE, 0);
         long double sum_sum_JDOS_M_spin = 0;
-        for (int i = 0; i < this->ising_lattice->NE; i++)
+        for (int i = 0; i < this->ising_lattice->NE; ++i)
         {
-            for (int j = 0; j < this->ising_lattice->line_size_Npos.at(q + 1); j++)
+            for (int j = 0; j < this->ising_lattice->line_size_Npos.at(q + 1); ++j)
                 sum_JDOS_M_spin[i] += this->JDOS_M_spin[q + 1][j * this->ising_lattice->NE + i];
             sum_sum_JDOS_M_spin += sum_JDOS_M_spin[i];
         }
 
-        for (int i = 0; i < this->ising_lattice->NE * this->ising_lattice->line_size_Npos.at(q + 1); i++)
+        for (int i = 0; i < this->ising_lattice->NE * this->ising_lattice->line_size_Npos.at(q + 1); ++i)
             this->JDOS_M_spin[q + 1][i] = this->JDOS_M_spin[q + 1][i] * exp(this->ising_lattice->norm_factor[q + 1]) / sum_sum_JDOS_M_spin;
 
-        for (int i = 0; i < this->ising_lattice->NE; i++)
+        for (int i = 0; i < this->ising_lattice->NE; ++i)
             sum_JDOS_M_spin[i] = 0;
         sum_sum_JDOS_M_spin = 0;
-        for (int i = 0; i < this->ising_lattice->NE; i++)
+        for (int i = 0; i < this->ising_lattice->NE; ++i)
         {
-            for (int j = 0; j < this->ising_lattice->line_size_Npos.at(q + 1); j++)
+            for (int j = 0; j < this->ising_lattice->line_size_Npos.at(q + 1); ++j)
                 sum_JDOS_M_spin[i] += this->JDOS_M_spin[q + 1][j * this->ising_lattice->NE + i];
             sum_sum_JDOS_M_spin += sum_JDOS_M_spin[i];
         }
 
-        for (int i = 0; i < this->ising_lattice->NE; i++)
+        for (int i = 0; i < this->ising_lattice->NE; ++i)
             this->ising_lattice->JDOS[i * this->ising_lattice->NM + q + 1] = sum_JDOS_M_spin[i] * exp(this->ising_lattice->norm_factor[q + 1]) / sum_sum_JDOS_M_spin;
 
         int hits = 0;
-        for (int i = 0; i < this->ising_lattice->NE * this->ising_lattice->line_size_Npos.at(q); i++)
+        for (int i = 0; i < this->ising_lattice->NE * this->ising_lattice->line_size_Npos.at(q); ++i)
             if (this->JDOS_M_spin[q][i] > 0)
                 hits++;
 
