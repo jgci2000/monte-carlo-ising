1421ced9b7d887d4989f5fa27d91e76282291640
João Inácio <jgci2000@hotmail.com>
2021-12-27 15:16:30 +0000

    fixed some issues regarding open mp

--- error_fss/error_fss.cpp
+++ error_fss/error_fss.cpp
@@ -14,14 +14,14 @@ int main(int argc, char** argv) {
     int skip_vals[] = {1, N_atm/4, N_atm/2, N_atm, 2*N_atm, 4*N_atm}; int n = 6;
     std::string skip_str[] = {"0", "0.25", "0.5", "1", "2", "4"};
     
-    omp_set_num_threads(6);
-    #pragma omp parallel for
+    omp_set_num_threads(run_max);
     for (int REP_exp = 2; REP_exp <= exp_REP_final; REP_exp++) {
         long long REP = pow(10, REP_exp);
         
         for (int skip_idx = 0; skip_idx < n; skip_idx++) {
             int skip = skip_vals[skip_idx];
 
+            #pragma omp parallel for
             for (int run = 0; run < run_max; run++) {
                 std::string path = "data/REP_" + std::to_string(REP_exp) + "/skip_" + skip_str[skip_idx] + "/";
                 std::string name = std::to_string(run) + "_JDOS.txt";
@@ -32,10 +32,9 @@ int main(int argc, char** argv) {
                 FSS fss(REP, skip, rng, ising);
                 fss.simulate(run, false);
                 fss.write_to_file(name, path, true);
-            }
+            } 
         }
     }    
-
-
+    
     return 0;
 }
